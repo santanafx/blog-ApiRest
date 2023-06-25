4bf9b332600cbafba8852b1724b11b64ef82e672
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-06-25 11:29:28 -0300

    Atualizado aparência de Data.jsx

--- src/pages/Data.jsx
+++ src/pages/Data.jsx
@@ -20,20 +20,22 @@ export const Data = () => {
   };
 
   return (
-    <section>
+    <section className="mt-5 max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
       {data !== undefined ? (
-        <>
+        <section>
+            <h1 className="text-4xl">Banco de dados:</h1>
+            <hr className="mt-2"/>
           {data.map((element) => (
-            <div key={element.id}>
+            <div key={element.id} className="mt-2 mb-5 max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
               <h1>{element.title}</h1>
               <div>
-                <p>{element.text}</p>
+              <textarea className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg" defaultValue={element.text}></textarea>
               </div>
-              <button onClick={() => handleDelete(element.id)}>Deletar</button>
-              <button onClick={() => navigate(`/updateData/${element.id}`)}>Edit</button>
+              <button className="mt-3 mr-3 px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150" onClick={() => handleDelete(element.id)}>Deletar</button>
+              <button className="mt-3 mr-3 px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150" onClick={() => navigate(`/updateData/${element.id}`)}>Edit</button>
             </div>
           ))}
-        </>
+        </section>
       ) : (
         "Loading..."
       )}
